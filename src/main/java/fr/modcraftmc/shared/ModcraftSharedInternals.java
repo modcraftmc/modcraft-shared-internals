1fbb6fcd28f6115e8c8b1b8f9cfd63d4d528cd87
manugame <manugameur.pro@gmail.com>
2023-07-18 19:21:01 +0200

    publish build to maven with shadowjar

--- src/main/java/fr/modcraftmc/shared/ModcraftSharedInternals.java
+++ src/main/java/fr/modcraftmc/shared/ModcraftSharedInternals.java
@@ -3,8 +3,6 @@ package fr.modcraftmc.shared;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.logging.LogManager;
-
 public class ModcraftSharedInternals {
     public static final Logger LOGGER = LoggerFactory.getLogger("ModcraftMC");
 
