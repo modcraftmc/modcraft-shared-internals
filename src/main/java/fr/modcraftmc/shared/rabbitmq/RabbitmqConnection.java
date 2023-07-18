dc7fd1d3d8da4fcf0c25be1bbfd90d341a704463
manugame <manugameur.pro@gmail.com>
2023-07-18 20:57:24 +0200

    WIP RabbitmqConnectionBuilder

--- src/main/java/fr/modcraftmc/shared/rabbitmq/RabbitmqConnection.java
+++ src/main/java/fr/modcraftmc/shared/rabbitmq/RabbitmqConnection.java
@@ -1,4 +1,41 @@
 package fr.modcraftmc.shared.rabbitmq;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import fr.modcraftmc.shared.ModcraftSharedInternals;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
 public class RabbitmqConnection {
+
+    private Connection connection;
+    private final List<Channel> channels = new ArrayList<>();
+
+    public RabbitmqConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    public Channel createChannel() throws IOException {
+        Channel channel = connection.createChannel();
+        channels.add(channel);
+        return channel;
+    }
+
+    public void close() {
+        for (Channel channel : channels) {
+            try {
+                channel.close();
+            } catch (IOException | TimeoutException e) {
+                ModcraftSharedInternals.LOGGER.error(String.format("Error while closing rabbitmq channel %s", e.getMessage()));
+            }
+        }
+        try {
+            connection.close();
+        } catch (IOException e) {
+            ModcraftSharedInternals.LOGGER.error(String.format("Error while closing rabbitmq connection %s", e.getMessage()));
+        }
+    }
 }
--- src/main/java/fr/modcraftmc/shared/rabbitmq/RabbitmqConnectionBuilder.java
+++ src/main/java/fr/modcraftmc/shared/rabbitmq/RabbitmqConnectionBuilder.java
@@ -1,4 +1,84 @@
 package fr.modcraftmc.shared.rabbitmq;
 
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Recoverable;
+import com.rabbitmq.client.RecoveryListener;
+import fr.modcraftmc.shared.ModcraftSharedInternals;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class RabbitmqConnectionBuilder {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+    private String virtualHost;
+    private Runnable onHeartbeatFailed;
+    private Runnable onHeartbeatSucceeded;
+
+    public RabbitmqConnectionBuilder() {
+    }
+
+
+    public RabbitmqConnectionBuilder host(String host) {
+        this.host = host;
+        return this;
+    }
+
+    public RabbitmqConnectionBuilder port(int port) {
+        this.port = port;
+        return this;
+    }
+
+    public RabbitmqConnectionBuilder username(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public RabbitmqConnectionBuilder password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public RabbitmqConnectionBuilder virtualHost(String virtualhost) {
+        this.virtualHost = virtualhost;
+        return this;
+    }
+
+    public RabbitmqConnectionBuilder onHeartbeatFailed(Runnable onHeartbeatFailed) {
+        this.onHeartbeatFailed = onHeartbeatFailed;
+        return this;
+    }
+
+    // Only triggered if heartbeat had failed before
+    public RabbitmqConnectionBuilder onHeartbeatSucceeded(Runnable onHeartbeatSucceeded) {
+        this.onHeartbeatSucceeded = onHeartbeatSucceeded;
+        return this;
+    }
+
+    public RabbitmqConnection build() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(host);
+        factory.setPort(port);
+        factory.setUsername(username);
+        factory.setPassword(password);
+        factory.setVirtualHost(virtualHost);
+        Connection connection = factory.newConnection();
+        ((Recoverable) connection).addRecoveryListener(new RecoveryListener() {
+            @Override
+            public void handleRecovery(Recoverable recoverable) {
+                ModcraftSharedInternals.LOGGER.warn("RabbitMQ server is back online");
+                onHeartbeatSucceeded.run();
+            }
+            @Override
+            public void handleRecoveryStarted(Recoverable recoverable) {
+                ModcraftSharedInternals.LOGGER.error("RabbitMQ server is unreachable");
+                onHeartbeatFailed.run();
+            }
+        });
+        return new RabbitmqConnection(connection);
+    }
 }
